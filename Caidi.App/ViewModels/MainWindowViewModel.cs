42033fc8b0b792bc92828e06e9061031cdd1e53e
tomkarho <tommi@tomkarho.com>
2021-03-06 10:01:41 +0200

    Add a couple of buttons to see how the ui responds

--- Caidi.App/ViewModels/MainWindowViewModel.cs
+++ Caidi.App/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using JetBrains.Annotations;
 
 namespace Caidi.App.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Hello World!";
+        public string FilesPath => "/home/tomkarho/Downloads/YT";
+
+        public void OnLoadFilesClick() => Console.WriteLine("Load files clicked");
+        public void OnExtractAudioClick() => Console.WriteLine("OnExtractAudioClick clicked");
     }
 }
