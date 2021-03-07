d6d5e6b3ac9d13414a33b5337a724ee2009ecdaf
tomkarho <tommi@tomkarho.com>
2021-03-07 19:40:41 +0200

    Add support for appsettings.json file

--- Caidi.App/App.xaml.cs
+++ Caidi.App/App.xaml.cs
@@ -1,16 +1,41 @@
+using System;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Caidi.App.Models;
 using Caidi.App.ViewModels;
 using Caidi.App.Views;
+using Microsoft.Extensions.Configuration;
 
 namespace Caidi.App
 {
     public class App : Application
     {
+        private readonly string ConfigFile = Environment.GetEnvironmentVariable("Environment") == "Development"
+            ? "appsettings.Development.json"
+            : "appsettings.json";
+        
+        private IConfiguration Configuration;
         public override void Initialize()
         {
+            LoadConfiguration();
+            Configure();
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void Configure()
+        {
+            
+        }
+
+        private void LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(ConfigFile, false);
+
+            Configuration = builder.Build();
         }
 
         public override void OnFrameworkInitializationCompleted()
